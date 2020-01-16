fd21a915d50cf09b00f06dd933e086a384d4d4a2
luiztictac <luiztictac@gmail.com>
2020-01-15 23:16:13 -0300

    mensagem

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 require_once("config.php");
 
+
+$user= new Usuario();
+$user-> loadbyId(1);
+echo $user;
+
+/*
 $sql = new Sql();
 
 $usuarios = $sql-> select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 
 ?>
