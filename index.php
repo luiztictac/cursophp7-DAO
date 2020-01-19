1f49c048ed1fe474beab54157dce7f2d8f56e586
luiztictac <luiztictac@gmail.com>
2020-01-18 23:14:18 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 require_once("config.php");
 
-
-$user= new Usuario();
-$user-> loadbyId(1);
-echo $user;
+//carrega usuaro
+//$user= new Usuario();
+//$user-> loadbyId(1);
+//echo $user;
 
 /*
 $sql = new Sql();
@@ -14,5 +14,19 @@ $usuarios = $sql-> select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
+//carrega uma lista de usuario
+//$lista = Usuario::getlist();
+
+//echo json_encode($lista);
+
+//carrega uma lista de usuarios buscando leo login
+//$search = Usuario::search("lo");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e senha
+$usuario = new Usuario();
+$usuario->login("user","12345");
+
+echo $usuario;
 
 ?>
